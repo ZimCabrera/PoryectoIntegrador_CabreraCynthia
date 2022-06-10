33c873906849dd4134efba5622d19e8ed405b4b3
Cynthia M. Cabrera (ZiM) <cynthiacabrera1088@gmail.com>
2022-06-09 22:23:11 -0300

    Generar componentes

--- ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
+++ ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPComponent,
+    RedesSocialesComponent
   ],
   imports: [
     BrowserModule,
