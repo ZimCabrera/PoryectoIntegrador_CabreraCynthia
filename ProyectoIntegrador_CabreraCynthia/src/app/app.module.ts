092c8178fd00fd24bd2c98f474bcbaa9bec58a62
ZiMCab <cynthiacabrera1088@gmail.com>
2022-06-13 22:13:23 -0300

    bootstrap

--- ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
+++ ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component';
 import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgPComponent,
-    RedesSocialesComponent
+    RedesSocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
