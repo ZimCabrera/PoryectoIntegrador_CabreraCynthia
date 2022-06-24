1c1e01f1c1c060134f6f203059f74a4c8f7934d4
ZiMCab <cynthiacabrera1088@gmail.com>
2022-06-24 20:40:46 -0300

    Nav

--- ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
+++ ProyectoIntegrador_CabreraCynthia/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
-import { BannerComponent } from './components/banner/banner.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component'
 import { NavComponent } from './components/nav/nav.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -13,13 +11,18 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { SkillComponent } from './components/skill/skill.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RedesSocialesComponent,
+    NavComponent,
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
     SkillComponent,
-    ProyectosComponent
+    ProyectosComponent,
   ],
   imports: [
     BrowserModule,
