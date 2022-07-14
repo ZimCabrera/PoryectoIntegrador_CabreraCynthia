9c2b582a20b0dad1a007d14c9b7ccdd1c3e5c0bc
ZiMCab <cynthiacabrera1088@gmail.com>
2022-07-14 18:47:17 -0300

    js

--- ProyectoIntegrador_CabreraCynthia/src/assets/js/main.js
+++ ProyectoIntegrador_CabreraCynthia/src/assets/js/main.js
@@ -4,7 +4,7 @@
 
      tabsContainer.addEventListener("click", (event) =>{
       if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active")){
-        const target = event.target.gettAtribute("data-target");
+        const target = event.target.getAttribute("data-target");
         tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
         event.target.classList.add("active", "outer-shadow");
         sobreMi.querySelector(".tab-content.active").classList.remove("active");
