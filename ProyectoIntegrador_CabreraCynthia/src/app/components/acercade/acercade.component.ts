5a0c34be1d94f847d10513808961c0ce7b0b5d3a
ZiMCab <cynthiacabrera1088@gmail.com>
2022-07-13 21:24:40 -0300

    details

--- ProyectoIntegrador_CabreraCynthia/src/app/components/acercade/acercade.component.ts
+++ ProyectoIntegrador_CabreraCynthia/src/app/components/acercade/acercade.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-acercade',
   templateUrl: './acercade.component.html',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcercadeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
