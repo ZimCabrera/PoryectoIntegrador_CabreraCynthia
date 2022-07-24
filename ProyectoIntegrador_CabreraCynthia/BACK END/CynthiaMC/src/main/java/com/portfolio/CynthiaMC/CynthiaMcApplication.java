e35531a77c1063890d5846cafb3b42d8e2d91136
ZiMCab <cynthiacabrera1088@gmail.com>
2022-07-23 21:22:59 -0300

    BackEnd

---
+++ ProyectoIntegrador_CabreraCynthia/BACK END/CynthiaMC/src/main/java/com/portfolio/CynthiaMC/CynthiaMcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.CynthiaMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CynthiaMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CynthiaMcApplication.class, args);
+	}
+
+}
