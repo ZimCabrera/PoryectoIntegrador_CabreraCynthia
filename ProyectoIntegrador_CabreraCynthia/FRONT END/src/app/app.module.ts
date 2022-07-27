95654576aa5b95366b628856792642b89d534a0f
ZiMCab <cynthiacabrera1088@gmail.com>
2022-07-26 23:22:36 -0300

    login

--- ProyectoIntegrador_CabreraCynthia/FRONT END/src/app/app.module.ts
+++ ProyectoIntegrador_CabreraCynthia/FRONT END/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     AcercadeComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
